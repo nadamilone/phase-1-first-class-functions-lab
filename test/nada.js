9ed6db489ede715cc493a6b9c241e50617fc0f9a
NADA MILONE <nadamilone@gmail.com>
2022-04-28 11:30:46 -0400

    commit tree 204087d3b748b8b84c17d2bfe50de6332ed63a9c

---
+++ test/nada.js
@@ -0,0 +1,48 @@
+// const add = (num1, num2) => num1 + num2;
+
+// const multiply = (num1, num2) => num1 * num2;
+
+// const subtract = (num1, num2) => num1 - num2;
+
+//
+
+
+// function compute(cb) {
+//     return cb(6, 3);
+// }
+// console.log(compute(add));
+// console.log(compute(subtract));
+// console.log(compute(multiply));
+
+// computer(3, 6) => 3 + 6;
+
+// const favorites = [pokemon[0], pokemon[1], "Squirtle", "Turtwig"];
+
+
+
+// / 1. Please complete the following function so that it will console.log each element of the accepted array, one at a time, as a numbered list. Should work for arrays of all lengths.
+// // please test your code before submitting!! 
+
+// // i.e.
+
+// // numberedList(["apple", "banana", "orange"])
+
+// // console output
+// // => 1. apple
+// // => 2. banana
+// // => 3. orange
+
+// function numberedList(array){
+//    // your code here
+// }
+
+
+// console.log(numberedList(["apple", "banana", "orange",4,6,88,9999]));
+
+// function numberedList(array){
+//     for(let i = 0; i < array.length; i++) {
+//         console.log(`${i + 1}. ${array[i]}`);
+//     }
+// }
+
+// console.log(numberedList(["apple", "banana", "orange"]));
